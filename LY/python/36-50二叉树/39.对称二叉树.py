9d116d69bff085ef6ea5a4493a07cbd8106f67d8
Teachmeplaycode <1262993264@qq.com>
2026-05-27 13:26:38 +0800

    打卡

---
+++ LY/python/36-50二叉树/39.对称二叉树.py
@@ -0,0 +1,18 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:return True
+        def isMirror(left: TreeNode, right: TreeNode) -> bool:
+            if not left and not right:return True
+            if not left or not right:return False
+            return (left.val == right.val) and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+        return isMirror(root.left, root.right)
+            
+root = TreeNode(1)
+sol = Solution()
+print(sol.isSymmetric(root))
