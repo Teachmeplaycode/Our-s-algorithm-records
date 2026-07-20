a7f5c889085e319e315d84adc37304383754d2d9
Teachmeplaycode <1262993264@qq.com>
2026-07-20 16:22:17 +0800

    打卡

---
+++ LY/python/81-动态规划/87.最长递增子序列.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [0] * (len(nums) + 1)
+        for i,num1 in enumerate(nums):
+            for j,num2 in enumerate(nums[:i]):
+                if num1 > num2:dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)
+sol = Solution()
+nums = [10,9,2,5,3,7,101,18]
+print(sol.lengthOfLIS(nums))
